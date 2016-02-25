8b0d606e3144adb1abe1441cd8dccba94028476f
smalltin95 <smalltin95@gmail.com>
2016-02-25 12:51:29 +0100

    message de validation

--- create_user.sh
+++ create_user.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
-echo "******************************"
-echo "Entrer une adresse IP"
+echo "****************************************"
+echo "Entrer l'adresse IP de la machine cible"
 read ADRESS_IP
 echo $ADRESS_IP > hosts
-echo "******************************"
 
-echo "******************************"
+echo "****************************************"
 echo "Entrer un nom d'utilisateur: "
 read USERNAME
-echo "******************************"
 
-echo "******************************"
+echo "****************************************"
 echo "Entrer un mot de passe: "
 read -s PASSWORD
-echo "******************************"
 
 ansible-playbook -i hosts create_user.yml --extra-vars "hosts=$ADRESS_IP username=$USERNAME password=$PASSWORD "  --ask-become
